f7ee89be49213450e08a5d27cef3cfa75459e7d2
Romek Goj <roman.goj@gmail.com>
2018-12-09 13:31:49 +0000

    Add lines for stages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { setClassMetadata } from '@angular/core/src/render3';
+import { listeners } from 'cluster';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,43 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'lm-joint-showcase';
   scale = 1000;
+
+  centreX = this.scale;
+  centreY = this.scale / 2;
+
+  circles = [
+    { title: '1' },
+    { title: '2' },
+    { title: '3' },
+    { title: '4' },
+    { title: '5' },
+    { title: '6' },
+    { title: '7' },
+    { title: '8' },
+    { title: '9' }
+  ]
+
+  radius = 450;
+
+  lines = [
+    { title: '1' },
+    { title: '2' },
+    { title: '3' },
+    { title: '4' },
+    { title: '8' },
+  ]
+
+  edgeX(index, length) {
+    const angle = index / length;
+    const x = this.radius * Math.sin(angle * 2 * Math.PI);
+    console.log(x);
+    return x;
+  }
+
+  edgeY(index, length) {
+    const angle = index / length;
+    const y = this.radius * Math.cos(angle * 2 * Math.PI);
+    console.log(y);
+    return y;
+  }
 }
